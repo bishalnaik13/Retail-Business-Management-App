cf1231e603e34db2c802bb472ecc57b6a5587c48
Bishal Naik <bishalbijaynaikdob2004@gmail.com>
2026-01-07 22:19:20 +0530

    invoice data model and auto invoice number logic

---
+++ src/models/Invoice.js
@@ -0,0 +1,23 @@
+
+
+export default class Invoice {
+    constructor({
+        invoiceNo,
+        date,
+        customerId,
+        items,
+        subtotal,
+        tax,
+        total,
+        paymentStatus,
+    }) {
+        this.invoiceNo = invoiceNo;
+        this.date = date;
+        this.customerId = customerId;
+        this.items = items;
+        this.subtotal = subtotal;
+        this.tax = tax;
+        this.total = total;
+        this.paymentStatus = paymentStatus; 
+    }
+}
